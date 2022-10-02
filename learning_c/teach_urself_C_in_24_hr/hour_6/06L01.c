66baf657078d2adeeb5c1b911337eff11cee602d
Uyi Obazee <utejoe.ju@gmail.com>
2022-10-02 22:22:47 +0100

    first commit for hour_6

---
+++ learning_c/teach_urself_C_in_24_hr/hour_6/06L01.c
@@ -0,0 +1,33 @@
+/*06L01.c: Using arithmetic assignment operators */
+#include <stdio.h>
+
+main()
+{
+	int x, y, z;
+	
+	x = 1;	/* intialize x */
+	y = 3; 	/* intialize y */
+	z = 10;	/* intialize z */
+	printf("Given x = %d, y = %d, and z = %d, \n", x, y, z);
+
+	x = x+y;
+	printf("x = x + y assigns %d to x; \n", x);
+	
+	x = 1; /* reset x */
+	x += y;
+	printf("x += y assigns %d to x;\n", x);
+	
+	x = 1; /* reset x */
+	z = z * z + y;
+	printf("z = z * x + y assigns %d to z;\n", z);\
+	
+	z = 10; /* reset z */
+	z *= x + y;
+	printf("z *= x + y assigns %d to z.\n", z);
+
+	z = 10; /* reset z */
+	z *= x + y;
+	printf("z *= x + y assigns %d to z.\n", z);
+
+	return 0;
+}
