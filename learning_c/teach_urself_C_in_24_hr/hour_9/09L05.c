ae4c0d70b8d9fe02b5d1f448e4c756fc2b793ef3
Uyi Obazee <utejoe.ju@gmail.com>
2022-10-12 09:10:47 +0100

    committing hour_9

---
+++ learning_c/teach_urself_C_in_24_hr/hour_9/09L05.c
@@ -0,0 +1,18 @@
+/* 09L05.c: Using pow() and sqrt() functions */
+#include <stdio.h>
+#include <math.h>
+
+main()
+{
+	double x, y, z;
+
+	x = 64.0;
+	y = 3.0;
+	z = 0.5;
+	printf("pow(64.0, 3.0) returns: %7.0f\n", pow(x, y));
+	printf("sqrt(64.0) returns: 	%2.0f\n", sqrt);
+	printf("pow(64.0, 0.5) returns:	%2.0f\n", pow(x, z));
+	return 0;
+
+
+	}
