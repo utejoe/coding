60f04bf0aaf7732915c900578823d506ebe686ee
Uyi Obazee <utejoe.ju@gmail.com>
2022-10-15 03:29:40 +0100

    First commit for hour 13

---
+++ learning_c/teach_urself_C_in_24_hr/hour_13/13L02.c
@@ -0,0 +1,19 @@
+/* 13L02.c: Measuring string length */
+#include <stdio.h>
+#include <string.h>
+
+main()
+{
+	char str1[] = {'A', ' ',
+		       's', 't', 'r', 'i', 'n', 'g', ' ',
+		       'c', 'o', 'n', 's', 't', 'a', 'n', 't', '\0'};
+	char str2[] = "Another string constant";
+	char *ptr_str = "Assign a string to a pointer.";
+
+	printf("The length of str1 is: %d bytes\n", strlen(str1));
+	printf("The length of str2 is: %d bytes\n", strlen(str2));
+	printf("The length of the string assigned to ptr_str is: %d bytes\n",
+		strlen(ptr_str));
+	return 0;
+}  /* The results indicate that the strlen() function does
+not count the null characters appended to the two string constants by the compiler, either */
