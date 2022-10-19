28fcde6a66ed606f035f8b49d7608cb3b79b283a
Uyi Obazee <utejoe.ju@gmail.com>
2022-10-19 22:59:00 +0100

    committing hour_18

---
+++ learning_c/teach_urself_C_in_24_hr/hour_18/18L02.c
@@ -0,0 +1,37 @@
+/* 18L02.c: Usimg the enum data type */
+#include <stdio.h>
+/* main() function */
+main()
+{
+	enum units{penny = 1,
+		   nickel = 5,
+		   dime = 10,
+		   quarter = 25,
+		   dollar = 100};
+	int money_units[5] = {
+		   dollar,
+		   quarter,
+		   dime,
+		   nickel,
+		   penny};
+	char *unit_name[5] = {
+		  "dollar(s)",
+		  "quarter(s)",
+		  "dime(s)",
+		  "nickel(s)",
+		  "penny(s)"};
+	int cent, tmp, i;
+
+	printf("Enter a monetary value in cents:\n");
+	scanf("%d", &cent);  /* get input from the user */
+	printf("Which is equivalent to:\n");
+	tmp = 0;
+	for (i=0; i<5; i++){
+	   tmp = cent / money_units[1];
+	   cent -= tmp * money_units[i];
+	   if (tmp)
+ 	     printf("%d %s ", tmp, unit_name[i]);
+	}
+	printf("\n");
+	return 0;
+}
