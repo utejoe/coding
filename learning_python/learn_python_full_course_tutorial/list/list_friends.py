825535e0aff1ef3a3e29cdc2d8bd2da02f8f2842
Uyi Obazee <utejoe.ju@gmail.com>
2022-11-20 12:21:58 +0100

    learing coding

---
+++ learning_python/learn_python_full_course_tutorial/list/list_friends.py
@@ -0,0 +1,4 @@
+friends = ['Kelvin', "Karen", "Jim"] # a list of friends
+print(friends)
+print(friends[1]) #printing a particular elements using index numbers. NB: indexing starts from 0.
+print(friends[-3])#printing a particular element using -ve indexing which starts from the ending of the line
