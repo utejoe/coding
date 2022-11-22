fafe9497a219365a35a47da16afd0a617dbd9ff6
Uyi Obazee <utejoe.ju@gmail.com>
2022-11-22 17:39:33 +0100

    learning coding

--- learning_python/learn_python_full_course_tutorial/tuples/tuples.py
+++ learning_python/learn_python_full_course_tutorial/tuples/tuples.py
@@ -1,3 +1,7 @@
-coordinates = (4, 5) #creating a coordinates tuple
+coordinates = (10, 3) #creating a coordinates tuple
 print(coordinates)
 print(coordinates[0]) #printing a particular item via its index
+co_ordinates = [(4, 5), (6, 7), (80, 34)] # a co_ordinate tuple with more than two items per index
+print (co_ordinates)
+print(co_ordinates[1], co_ordinates[0]) #printing multiple tuple items via indexing
+ 
